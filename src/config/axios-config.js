84e8b9b499949d35071475d02a8597d53611ebc4
Ernestas.ivanovas <ernestas1ivanovas@gmail.com>
2022-02-13 14:48:42 +0200

    Revert "axios ip"
    This reverts commit 14f7df291ae420e45be8c692ab9f3933551298ad.

--- src/config/axios-config.js
+++ src/config/axios-config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACK_END_URL = "https://word-counter-back.herokuapp.com/";
+const BACK_END_URL = "http://localhost:8080";
 
 let Axios =  axios.create({
   baseURL: BACK_END_URL,
